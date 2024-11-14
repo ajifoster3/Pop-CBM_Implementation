15587bf59cb644926e98c12bfd78515fd7c61d72
Ash <ajifoster3@gmail.com>
2024-11-14 17:48:13 +0000

    Add one_move unit test

--- test_Operator.py
+++ test_Operator.py
@@ -38,6 +38,7 @@ class TestOperatorFunctions(TestCase):
                        [1, 0, 9, 8],
                        [8, 9, 0, 1],
                        [9, 8, 0.5, 0]]
+
         solution_task_counts = [2, 1]
         solution_task_order = [0, 1, 3]
         OperatorFunctions.find_best_task_position(
@@ -52,6 +53,7 @@ class TestOperatorFunctions(TestCase):
                        [1, 0, 9, 8],
                        [0.5, 9, 0, 9],
                        [9, 8, 9, 0]]
+
         solution_task_counts = [2, 1]
         solution_task_order = [0, 1, 3]
         OperatorFunctions.find_best_task_position(
@@ -76,7 +78,27 @@ class TestOperatorFunctions(TestCase):
         self.fail()
 
     def test_one_move(self):
-        self.fail()
+        solution_task_counts = [2, 2]
+        solution_task_order = [0, 1, 3, 2]
+        solution = [solution_task_order, solution_task_counts]
+        cost_matrix = [[0, 1, 8, 9],
+                       [1, 0, 9, 8],
+                       [8, 9, 0, 0.5],
+                       [9, 8, 1, 0]]
+        task_order, task_counts = OperatorFunctions.one_move(solution, cost_matrix)
+        self.assertEqual(task_order, [0, 1, 2, 3])
+        self.assertEqual(task_counts, [2, 2])
+
+        solution_task_counts = [2, 2]
+        solution_task_order = [0, 1, 3, 2]
+        solution = [solution_task_order, solution_task_counts]
+        cost_matrix = [[0, 1, 8, 9],
+                       [1, 0, 9, 8],
+                       [0.5, 9, 0, 9],
+                       [9, 8, 9, 0]]
+        task_order, task_counts = OperatorFunctions.one_move(solution, cost_matrix)
+        self.assertEqual(task_order, [2, 0, 1, 3])
+        self.assertEqual(task_counts, [3, 1])
 
     def test_two_swap(self):
         self.fail()
