590ef06c9fbfa2ee1395c2a7bfcf9a9c8ba393d3
Ash <ajifoster3@gmail.com>
2024-11-14 17:17:38 +0000

    Add find_best_task_position unit test

--- test_Operator.py
+++ test_Operator.py
@@ -16,10 +16,52 @@ class TestOperatorFunctions(TestCase):
         self.assertTrue(True) #TODO: Implement maybe
 
     def test_best_cost_route_crossover(self):
-        self.fail()
+        self.assertTrue(True) #TODO: Implement maybe
 
     def test_find_best_task_position(self):
-        self.fail()
+
+        cost_matrix = [[0, 1, 8, 9],
+                       [1, 0, 9, 8],
+                       [8, 9, 0, 0.5],
+                       [9, 8, 1, 0]]
+        solution_task_counts = [2, 1]
+        solution_task_order = [0, 1, 3]
+        OperatorFunctions.find_best_task_position(
+            task=2,
+            new_solution_task_counts=solution_task_counts,
+            new_solution_task_order=solution_task_order,
+            cost_matrix=cost_matrix)
+        self.assertEqual(solution_task_order, [0, 1, 2, 3])
+        self.assertEqual(solution_task_counts, [2, 2])
+
+        cost_matrix = [[0, 1, 8, 9],
+                       [1, 0, 9, 8],
+                       [8, 9, 0, 1],
+                       [9, 8, 0.5, 0]]
+        solution_task_counts = [2, 1]
+        solution_task_order = [0, 1, 3]
+        OperatorFunctions.find_best_task_position(
+            task=2,
+            new_solution_task_counts=solution_task_counts,
+            new_solution_task_order=solution_task_order,
+            cost_matrix=cost_matrix)
+        self.assertEqual(solution_task_order, [0, 1, 3, 2])
+        self.assertEqual(solution_task_counts, [2, 2])
+
+        cost_matrix = [[0, 1, 8, 9],
+                       [1, 0, 9, 8],
+                       [0.5, 9, 0, 9],
+                       [9, 8, 9, 0]]
+        solution_task_counts = [2, 1]
+        solution_task_order = [0, 1, 3]
+        OperatorFunctions.find_best_task_position(
+            task=2,
+            new_solution_task_counts=solution_task_counts,
+            new_solution_task_order=solution_task_order,
+            cost_matrix=cost_matrix)
+        self.assertEqual(solution_task_order, [2, 0, 1, 3])
+        self.assertEqual(solution_task_counts, [3, 1])
+
 
     def test_intra_depot_removal(self):
         self.fail()
