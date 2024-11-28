b9291115a125dfedb6803315255216bae41e8d65
Ash <ajifoster3@gmail.com>
2024-11-28 17:47:18 +0000

    Include OpenAI Client for each of the ReEvo Classes

--- cbm_pop/ReEvo/ElitistMutation.py
+++ cbm_pop/ReEvo/ElitistMutation.py
@@ -1,3 +1,12 @@
+import os
+from openai import OpenAI
+
 class ElitistMutation:
     def __init__(self):
-        pass
+        def __init__(self):
+            self.client = OpenAI(
+                api_key=os.environ['CBM_POP_APIKEY'],
+                organization='org-aHxs2hPTZTYEPh8GyXZaDPmI',
+                project='proj_bMwxaxkpiKADKECQtHOCCUjh',
+            )
+            pass
